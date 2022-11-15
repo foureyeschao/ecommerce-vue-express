965539cea70bf77c2287f8a7551308ef7233b8a0
Yuhang Cao <foureyeschao@gmail.com>
2022-11-15 11:05:39 -0500

    update routes

--- server/app/routes/product.model.js
+++ server/app/routes/product.model.js
@@ -3,6 +3,18 @@ module.exports = app => {
 
     const router = require('express').Router()
 
+    //insert new data
+    router.post('/', product.create)
+    // retrieve all data
+    router.get('/', product.findAll)
+    //get one data
+    router.get('/:id', product.findOne)
+    //updat data
+    router.put('/:id', product.update)
+    //delete by id
+    router.delete('/:id', product.delete)
+
+
     router.get('/', product.findAll)
 
     app.use('/api/product', router)
